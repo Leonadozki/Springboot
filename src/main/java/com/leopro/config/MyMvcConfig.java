9e896c89c75a1b74e60a32854d829a673dca8d1f
leo <272137499@qq.com>
2019-10-22 23:19:11 +0800

    fix 多个页面静态资源拦截问题

--- src/main/java/com/leopro/config/MyMvcConfig.java
+++ src/main/java/com/leopro/config/MyMvcConfig.java
@@ -14,13 +14,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
-    // 注册视图控制器，去掉页面的静态资源拦截
+    // 注册视图控制器
     public void addViewControllers(ViewControllerRegistry registry){
         //   redirect路径转发到success.html
         registry.addViewController("/redirect").setViewName("success");
         registry.addViewController("/main").setViewName("dashboard");
-        registry.addViewController("/apis").setViewName("dashboard");
-        registry.addViewController("/api").setViewName("dashboard");
     }
 
     // 注册拦截器
@@ -28,6 +26,15 @@ public class MyMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
                 .excludePathPatterns("/", "/index", "/asserts/**",
                         "/login", "/webjars/**", "/user/login", "/css/**");
+    }
+
+    // 注册资源处理器，去掉页面的静态资源拦截
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 静态文件
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        // webjar文件
+        registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
     }
 
 }
